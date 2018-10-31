d4058496342000cdb1a1f7d1c4537d4da058e6e0
Md Sumsuddin <sumsuddinshojib@gmail.com>
2018-11-01 00:08:02 +0600

    edited gitignore and bash script for building lib faster

--- libs/build_dependencies.sh
+++ libs/build_dependencies.sh
@@ -9,18 +9,18 @@ install_cpprestsdk(){
 	restsdkDir="$libDir/cpprestsdk"
 	restsdkBuildDir="$restsdkDir/build.release"
    
-   if [ -d "$restsdkDir" ]; then
-      rm -rf "$restsdkDir"
+   if ! [ -d "$restsdkDir" ]; then
+        git clone https://github.com/Microsoft/cpprestsdk.git "$restsdkDir"
    fi
    
-	git clone https://github.com/Microsoft/cpprestsdk.git "$restsdkDir"
+
 	(cd $restsdkDir && git checkout tags/$RESTSDK_VERSION -b $RESTSDK_VERSION)
 	mkdir "$restsdkBuildDir"
 	if [[ "$OSTYPE" == "linux-gnu" ]]; then
-		export CXX=g++-4.9
+		export CXX=g++
 	fi
 	(cd "$restsdkBuildDir" && cmake ../Release -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_SAMPLES=OFF)
-	(cd "$restsdkBuildDir" && make)
+	(cd "$restsdkBuildDir" && make -j$(nproc))
 }
 
 mkdir -p "$libDir"
